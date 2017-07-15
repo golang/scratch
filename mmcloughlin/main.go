5566a5875b9da19341c257ec554e151d5c40d030
Michael McLoughlin <mmcloughlin@gmail.com>
2017-07-15 14:01:19 -0600

    mmcloughlin: main.go for Gophercon
    Change-Id: I1cc844fa35f76fe0da2a34257e307662456e4e25
    Reviewed-on: https://go-review.googlesource.com/48905
    Reviewed-by: Joe Tsai <thebrokentoaster@gmail.com>

---
+++ mmcloughlin/main.go
@@ -0,0 +1,12 @@
+// Copyright 2017 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The mmcloughlin command trolls America.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Make America Great Britain Again!!")
+}
