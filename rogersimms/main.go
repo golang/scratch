fcc8e9c124eb4aef02b1425d716c2c6209b187cb
Roger Simms <roger.simms@gmail.com>
2017-07-15 14:02:56 -0600

    rogersimms: add main.go
    Change-Id: Ice8ef8cf5963f92e07e978db04ab4768ebb2b517
    Reviewed-on: https://go-review.googlesource.com/48978
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

---
+++ rogersimms/main.go
@@ -0,0 +1,12 @@
+// Copyright 2017 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The rogersimms command is optimistic about Linux on the desktop.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("this is the year of linux on the desktop!")
+}
