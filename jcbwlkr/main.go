2136ea2ac87989e6b18d72cd9833c3cb3cfff9a1
Jacob Walker <jacobwalker0814@gmail.com>
2017-07-15 13:53:39 -0600

    jcbwlkr: add main.go for Gophercon workshop
    Change-Id: Icce187518137e58828e258e9e55e7a956d3ac6d9
    Reviewed-on: https://go-review.googlesource.com/48896
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

---
+++ jcbwlkr/main.go
@@ -0,0 +1,13 @@
+// Copyright 2017 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i < 11; i++ {
+		fmt.Println("Hello, Gerrit!")
+	}
+}
