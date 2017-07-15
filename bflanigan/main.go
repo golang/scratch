d79e548a5db41e5181c141f0d8b961adee5414c2
Brian Flanigan <brian_flanigan@cable.comcast.com>
2017-07-15 11:44:21 -0600

    bflanigan: add main.go Gophercon greeting
    Change-Id: I348f30eb4dc750b634c4cb6be32b5eab3570480b
    Reviewed-on: https://go-review.googlesource.com/48854
    Reviewed-by: Han-Wen Nienhuys <hanwen@google.com>

---
+++ bflanigan/main.go
@@ -0,0 +1,11 @@
+// Copyright 2017 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Thank you Go team for helping me to get my job done faster!")
+}
