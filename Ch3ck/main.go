cea466041e6e7f59490561a517faa183c0e26b9a
Nyah Check <check.nyah@gmail.com>
2017-07-15 14:05:22 -0600

    Ch3ck: Command states the status of containers.
    Change-Id: I9440b366f3eca69e4e803596cb5f0c95a51947d7
    Reviewed-on: https://go-review.googlesource.com/48953
    Reviewed-by: Jessie Frazelle <me@jessfraz.com>

---
+++ Ch3ck/main.go
@@ -0,0 +1,12 @@
+// Copyright 2017 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Ch3ck command states its opinion on containers.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("containers rule!")
+}
