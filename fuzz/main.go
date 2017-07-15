5d3fd457b499e8c94806146e39f5490b1dee703a
Patrick Bennett <patrick@thebennetts.com>
2017-07-15 14:58:47 -0600

    fuzz: test addition of dummy main
    Change-Id: I89d27cafbd3e5ac69b8eda82b9bad202747155c1
    Reviewed-on: https://go-review.googlesource.com/48963
    Reviewed-by: Kevin Burke <kev@inburke.com>

---
+++ fuzz/main.go
@@ -0,0 +1,12 @@
+// Copyright 2017 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The fuzz program is a simple scratch program for review purposes
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("containers rule!")
+}
