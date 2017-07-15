add5a2bbc1a0aacbfdeeea9f16e322fcdc1dc00a
Evan Hicks <evan.hicks2@gmail.com>
2017-07-15 11:14:45 -0600

    evanh: add a main.go for Gophercon
    Change-Id: I14a73391c0dde0b3020ba803c855fd1eaa5cb968
    Reviewed-on: https://go-review.googlesource.com/48752
    Reviewed-by: Jessica Frazelle <me@jessfraz.com>
    Reviewed-by: Chris Broadfoot <cbro@golang.org>

---
+++ evanh/main.go
@@ -0,0 +1,12 @@
+// Copyright 2017 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Fear leads to anger. Anger leads to hate. Hate leads to suffering.")
+	fmt.Println("Do or do not. There is no try.")
+}
