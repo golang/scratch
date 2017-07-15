d510abe60a50a0773e85891d1bc74a8a0890e8b5
Michael Matloob <matloob@golang.org>
2017-07-15 11:32:34 -0600

    matloob: add a main file
    Tools are the best!
    
    Change-Id: If0f3772def8f05c1c54957bef11ff01036783de4
    Reviewed-on: https://go-review.googlesource.com/48789
    Reviewed-by: Jessica Frazelle <me@jessfraz.com>

---
+++ matloob/main.go
@@ -0,0 +1,10 @@
+// Copyright 2014 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// This tool proclaims the ruliness of tools.
+package main
+
+func main() {
+	fmt.Println("Tools Rule!")
+}
