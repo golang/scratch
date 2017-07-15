67eb86861271af8da2785e968a59982c889464a7
diana ortega <dicaormu@gmail.com>
2017-07-15 14:18:01 -0600

    dicaormu: add main.go for GopherCon
    Change-Id: I35ad3166d199d796a9da4c728e3781eb62983e2f
    Reviewed-on: https://go-review.googlesource.com/48983
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

---
+++ dicaormu/main.go
@@ -0,0 +1,14 @@
+// Copyright 2017 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*Package main has the main code of the example
+for contributing to go, in the gophercon
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("containers rule!")
+}
