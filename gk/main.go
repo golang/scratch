6043ddcc646dd1b916a00892b91541ce67654543
Guillaume Koenig <guillaume.edward.koenig@gmail.com>
2017-07-15 19:24:07 +0200

    gk: add main.go for Gophercon
    Change-Id: I2d4f87a2a96afcc766bf272e83aa10eb8ae96c33
    Reviewed-on: https://go-review.googlesource.com/48770
    Reviewed-by: huadcu sulivan <huadcu@gmail.com>
    Reviewed-by: Chris Broadfoot <cbro@golang.org>

---
+++ gk/main.go
@@ -0,0 +1,11 @@
+// Copyright 2017 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Meh Linux doesn't have its year on the desktop, but it does power the Internet!")
+}
