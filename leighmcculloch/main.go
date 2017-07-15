879090b3aeafba21ef20caf910c625a38f3ecf82
Leigh McCulloch <leighmcc@gmail.com>
2017-07-15 17:25:54 +0000

    leighmcculloch: add a first contribution
    What
    ===
    Add a first contribution to the scratch repo.
    
    Why
    ===
    Because I want to contribute to Go and I'm in the GopherCon Go
    Contributor Workshop!
    
    Change-Id: Iffd46054cc6ca993d71e8572d37f3d505380ccd9
    Reviewed-on: https://go-review.googlesource.com/48814
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

---
+++ leighmcculloch/main.go
@@ -0,0 +1,12 @@
+// Copyright 2017 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("HI! This is my first contribution to a googlesource repo using gerrit.")
+	fmt.Println("╰(◕ヮ◕)つ¤=[]———")
+}
