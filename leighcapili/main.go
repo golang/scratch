9e3b27b55a59985cd3f7530a9e2ac095131fb0ee
leigh schrandt <leigh@null.net>
2017-07-15 11:18:53 -0600

    leighcapili: add gopher, leighcapili, to repo
    Change-Id: Ie071d374bcfa62394816ec5af7cf38f2f1a381a8
    Reviewed-on: https://go-review.googlesource.com/48784
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

---
+++ leighcapili/main.go
@@ -0,0 +1,13 @@
+// Copyright 2017 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Prints Leigh's views on the world
+// tw: @capileigh / gh: stealthybox
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("life is beautiful because it has dogs.")
+}
