8f1b81864e50e3808c67703aaf577c55ea5e7571
FourSeventy <msiggy@gmail.com>
2017-07-15 14:10:21 -0600

    main: Add production ready hello world function
    Change-Id: I7c3d7288d8a180954f4aae8da032f21f8baeb108
    Reviewed-on: https://go-review.googlesource.com/48973
    Reviewed-by: Joe Tsai <thebrokentoaster@gmail.com>

---
+++ msiggy/main.go
@@ -0,0 +1,14 @@
+// Copyright 2017 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//Package main is used to print a welcome message
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello Gophercon!")
+}
