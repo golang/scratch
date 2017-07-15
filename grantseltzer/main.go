79b6939b1492455bd21162a10aa0c0224e50fef5
Author: grantseltzer <grantseltzer@gmail.com>
2017-07-15 13:23:27 -0400

    grantseltzer: add main.go for Gophercon
    Change-Id: I00dd1d941d28682d33db888f9997fade686e3e93
    Signed-off-by: grantseltzer <grantseltzer@gmail.com>
    Reviewed-on: https://go-review.googlesource.com/48781
    Reviewed-by: Jessica Frazelle <me@jessfraz.com>

---
+++ grantseltzer/main.go
@@ -0,0 +1,14 @@
+// Copyright 2017 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Printf("%d is the year of linux on the desktop\n", time.Now().Year())
+}
