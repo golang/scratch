1874380bb91c3591c26d8a5996d54308970c2a54
Jacob Kobernik <jkobernik@gmail.com>
2017-07-15 11:42:45 -0600

    jakoberni: add myself
    Change-Id: I682c40ea93a5cbd3165a6503dbc4ad195834eab3
    Reviewed-on: https://go-review.googlesource.com/48852
    Reviewed-by: Chris Broadfoot <cbro@golang.org>
    Run-TryBot: Chris Broadfoot <cbro@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

---
+++ jakobernik/main.go
@@ -0,0 +1,13 @@
+// Copyright 2017 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("into orbit!")
+	fmt.Println("Now with legality!")
+	fmt.Println("time doesn't matter...")
+}
