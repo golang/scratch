9f29a4fc466c2a7bd7e8d1b10517b74a3172e1c7
Tom Elliott <tom.w.elliott@gmail.com>
2017-07-15 11:20:51 -0600

    telliott: add main.go for Gophercon
    Change-Id: I5f580ef88a9b3133f383fa7974ec9a21ee4cb7a1
    Reviewed-on: https://go-review.googlesource.com/48750
    Reviewed-by: Kevin Burke <kev@inburke.com>

---
+++ telliott/main.go
@@ -0,0 +1,11 @@
+// Copyright 2017 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("You build everything three times. Once to figure out what you want. Once to figure out how to do it. And once to do it.")
+}
