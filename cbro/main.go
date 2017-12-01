b35133a336d0b8cd60fdde6b816e99e92d3893db
Chris Broadfoot <cbro@golang.org>
2017-11-30 16:18:51 -0800

    cbro: add gopher art
    Change-Id: I7f2e62fe3e278701c03a2603124ba6f51de590f9
    Reviewed-on: https://go-review.googlesource.com/81278
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

---
+++ cbro/main.go
@@ -0,0 +1,31 @@
+// Copyright 2017 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print(`
+            ____  ___
+           / ___|/ _ \
+          | |  _| | | |
+          | |_| | |_| |
+           \____|\___/
+
+         ,_---~~~~~----._
+  _,,_,*^____      _____''*g*\"*,
+ / __/ /'     ^.  /      \ ^@q   f
+[  @f | @))    |  | @))   l  0 _/
+ \'/   \~____ / __ \_____/    \
+  |           _l__l_           I
+  }          [______]           I
+  ]            | | |            |
+  ]             ~ ~             |
+  |                            |
+   |                           |
+
+ Credit: https://gist.github.com/belbomemo/b5e7dad10fa567a5fe8a
+`)
+}
