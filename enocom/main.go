c0b20f22e1afb2589459d4b5e9a38de360093709
Eno Compton <enocom@google.com>
2018-03-08 14:02:53 -0700

    enocom: add Tang poem
    Change-Id: I7989ddc90269d5438faf448ad1aaa272e44d4f7e
    Reviewed-on: https://go-review.googlesource.com/99619
    Reviewed-by: Kevin Burke <kev@inburke.com>

---
+++ enocom/main.go
@@ -0,0 +1,17 @@
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import "fmt"
+
+// Meng Haoran 孟浩然 (689-740), "Chun xiao" 春曉
+var poem = `春眠不覺曉
+處處聞啼鳥
+夜來風雨聲
+花落知多少`
+
+func main() {
+	fmt.Println(poem)
+}
