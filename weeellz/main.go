c133e1159e7dc699f129c90ea73037203313f20c
weeellz <weeellz12@gmail.com>
2018-04-07 15:57:43 +0300

    weeellz: add main.go
    Change-Id: I26f2278b851e6ef0a74bc7b6bac706a9ad3b272b
    Reviewed-on: https://go-review.googlesource.com/105375
    Run-TryBot: Daniel Martí <mvdan@mvdan.cc>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Daniel Martí <mvdan@mvdan.cc>

---
+++ weeellz/main.go
@@ -0,0 +1,9 @@
+// Copyright 2018 The Go Authors. All Rights Reserved.
+// Use of this source code is governed by a BSD-style
+// licence that can be found in the LICENSE file.
+
+package main
+
+func main() {
+	println("Go is awesome")
+}
