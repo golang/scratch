d7c81c814e591a9770bc539f65c0772650e9b25b
Liz Rice <liz@lizrice.com>
2018-06-20 19:22:09 +0100

    lizrice: experiment with committing to scratch
    This is, y'know, practising, but this time I have something that compiles.
    
    Change-Id: I51d66c9184728964e70b140164646aaa46acadad
    Reviewed-on: https://go-review.googlesource.com/120117
    Reviewed-by: Daniel Martí <mvdan@mvdan.cc>
    Run-TryBot: Daniel Martí <mvdan@mvdan.cc>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

---
+++ lizrice/main.go
@@ -0,0 +1,9 @@
+// Also I want to see what happens if I miss the copyright
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("I could break this in countless ways like omitting the quote but go-fmt catches that.")
+}
