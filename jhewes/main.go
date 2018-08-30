e9e02ca7ab0f02bf93f0247304a35101627af2cb
Jason Hewes <jasonhewes5@gmail.com>
2018-08-30 14:24:56 -0400

    jhewes: changed main.go for Gophercon
    Change-Id: I42fef59246966c11f541d2317f0b0e8ea97df063
    Reviewed-on: https://go-review.googlesource.com/132319
    Reviewed-by: Ross Light <light@google.com>

---
+++ jhewes/main.go
@@ -0,0 +1,12 @@
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The jhewes program prints an amazing message.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("this is a change!")
+}
