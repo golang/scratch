b9918d00c0434c7911fc60757460e0faff5916ca
Drew Flower <drewvanstone@gmail.com>
2018-08-30 12:53:48 -0600

    drewvanstone: add printing example for Gophercon 2018
    Change-Id: I082695e971d4d614cbee15cedeae6095ab9a184c
    Reviewed-on: https://go-review.googlesource.com/132265
    Run-TryBot: Ian Lance Taylor <iant@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Ian Cottrell <iancottrell@google.com>

---
+++ drewvanstone/main.go
@@ -0,0 +1,14 @@
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// This tool proclaims the ruliness of tools.
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello Gophercon2018")
+}
