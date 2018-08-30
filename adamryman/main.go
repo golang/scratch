3b3aedbe49f8dc7335d30d8b101c09b6017f3b93
Adam Ryman <adamryman@gmail.com>
2018-08-30 12:56:36 -0700

    adamryman: add quine
    Change-Id: I2e6a437d0a1de69a44b9e172bf09a44bc57c2884
    Reviewed-on: https://go-review.googlesource.com/132345
    Reviewed-by: Kevin Burke <kev@inburke.com>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>
    Run-TryBot: Kevin Burke <kev@inburke.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

---
+++ adamryman/main.go
@@ -0,0 +1,17 @@
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+func main() { grave := string(96); print(p, grave, p, grave, "\n") }
+
+const p = `// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+func main() { grave := string(96); print(p, grave, p, grave, "\n") }
+
+const p = `
