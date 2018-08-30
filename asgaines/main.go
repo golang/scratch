081c3fdcdc9ded8ee7052c691be81f2124674b17
asgaines <andrew.s.gaines@gmail.com>
2018-08-30 15:08:59 -0600

    asgaines: add printing example for Gophercon
    Change-Id: I15e012f3f97dfd7de21534d6629eeaf802b14d10
    Reviewed-on: https://go-review.googlesource.com/132347
    Run-TryBot: Ian Lance Taylor <iant@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

---
+++ asgaines/main.go
@@ -0,0 +1,32 @@
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print(`
+            ____  ___
+           / ___|/ _ \
+          | |  _| | | |
+          | |_| | |_| |
+           \____|\___/
+
+				!
+         ,_---~~~~~----._
+  _,,_,*^____      _____''*g*\"*,
+ / __/ /'     ^.  /      \ ^@q   f
+[  @f | @))    |  | @))   l  0 _/
+ \'/   \~____ / __ \_____/    \
+  |           _l__l_           I
+  }          [______]           I
+  ]            | | |            |
+  ]             ~ ~             |
+  |                            |
+   |                           |
+
+ Credit: https://gist.github.com/belbomemo/b5e7dad10fa567a5fe8a
+`)
+}
