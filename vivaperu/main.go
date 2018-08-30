878e5d9ff28cfc6ce8fd6bb333b5cae4ac01664c
Gustavo Picón <tabo@tabo.pe>
2018-08-30 12:50:54 -0600

    vivaperu: added a most Peruvian tool
    Change-Id: I24bfdb4179c660eb2fe283284720af31be7309ea
    Reviewed-on: https://go-review.googlesource.com/132341
    Run-TryBot: Ian Lance Taylor <iant@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

---
+++ vivaperu/main.go
@@ -0,0 +1,14 @@
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// This tool asserts the existence of the Proud Nation of Peru.
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Viva Peru! 🇵🇪")
+}
