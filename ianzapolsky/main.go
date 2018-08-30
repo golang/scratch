92a85df99857568c5395ca9a664e763b6a59ebf5
ianzapolsky <ianzapolsky@gmail.com>
2018-08-30 12:11:35 -0600

    ianzapolsky: add a hello world program
    Change-Id: Idb195c985d1e4546ef56a82fd5282852a1e8010f
    Reviewed-on: https://go-review.googlesource.com/132266
    Reviewed-by: Kevin Burke <kev@inburke.com>
    Run-TryBot: Kevin Burke <kev@inburke.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

---
+++ ianzapolsky/main.go
@@ -0,0 +1,13 @@
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// ianzapolsky is a simple hello world program for review purposes.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello scratch!")
+	fmt.Println("this is a change!")
+}
