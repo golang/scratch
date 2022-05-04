54a22f34430482ff24b09aa75aa9307f149f0807
Than McIntosh <thanm@google.com>
2022-05-04 11:16:08 -0400

    scratch: some edits
    Adding some stuff.
    
    Change-Id: Iab6bca276286893a101569384e07b36cc565384f
    Reviewed-on: https://go-review.googlesource.com/c/scratch/+/403681
    Reviewed-by: Than McIntosh <thanm@google.com>

---
+++ thanm/main.go
@@ -0,0 +1,35 @@
+// Copyright 2022 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+func quote1() {
+	println("Obfuscate the pancakes!")
+	println()
+}
+
+func quote2() {
+	println("From the label of a secure disk: Nemo me impune accessit")
+	println()
+}
+
+func quote3() {
+	println("MY SENSORS INDICATE TRACE AMOUNTS OF CHOCOLATE IN THE PANTRY.  PLEASE LOAD")
+	println("SOME IN MY SCOOP FOR ANALYSIS.")
+	println("No, you'll spoil your appetite.")
+	println("MY MISSION MUST NOT FAIL.  PREPARE FOR ANNIHILATION, PITIFUL EARTH FEMALE.")
+	println()
+}
+
+func quote4() {
+	println("Never send a bunny to do a duck's job.")
+	println()
+}
+
+func main() {
+	quote1()
+	quote2()
+	quote3()
+	quote4()
+}
